98ea513875811645d8cb2d29f198613e5d1d46a6
WTabbron <will.tabbron@hotmail.co.uk>
2020-06-17 02:48:01 +0100

    drafts and publish

--- blog/views.py
+++ blog/views.py
@@ -17,12 +17,17 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            #post.published_date = timezone.now() //removed for adding publish button
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_list')
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -37,4 +42,14 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
 # Create your views here.
