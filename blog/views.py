25417cfa51d4c1d2654f40f455c600c900f7ac0b
WTabbron <will.tabbron@hotmail.co.uk>
2020-06-18 17:13:07 +0100

    fixed drafts bug

--- blog/views.py
+++ blog/views.py
@@ -11,6 +11,10 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+def draft_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/draft_detail.html', {'post': post})
+
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -65,6 +69,19 @@ def post_edit(request, pk):
             post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def draft_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk) #gets the post to be edited
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post) #opens the post to be edited
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('draft_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
